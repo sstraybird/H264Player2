958a7930caddc9a1d494b552cd74376a7d9df550
zhanghonggang <zhanghonggang@sinovoice.com.cn>
2022-11-29 17:45:55 +0800

    解析H264

--- app/src/main/java/com/example/h264player2/MainActivity.java
+++ app/src/main/java/com/example/h264player2/MainActivity.java
@@ -1,14 +1,77 @@
 package com.example.h264player2;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.Settings;
+import android.util.Log;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.widget.Toast;
+
+import java.io.File;
 
 public class MainActivity extends AppCompatActivity {
+    private static final int REQUEST_CODE = 1024;
 
+
+    public void checkPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            // 先判断有没有权限
+            if (Environment.isExternalStorageManager()) {
+                doSomething();
+            } else {
+                Intent intent = new Intent(Settings.ACTION_MANAGE_APP_ALL_FILES_ACCESS_PERMISSION);
+                intent.setData(Uri.parse("package:" + this.getPackageName()));
+                startActivityForResult(intent, REQUEST_CODE);
+            }
+        }
+    }
+    /**
+     * 模拟文件写入
+     */
+    private void doSomething() {
+        Toast.makeText(this, "Android VERSION  R OR ABOVE，HAVE MANAGE_EXTERNAL_STORAGE GRANTED!", Toast.LENGTH_LONG).show();
+    }
+    H264Player h264Player;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        checkPermission();
+        initSurface();
+
+    }
+
+    private void initSurface() {
+        SurfaceView surface = (SurfaceView) findViewById(R.id.preview);
+        final SurfaceHolder surfaceHolder = surface.getHolder();
+        surfaceHolder.addCallback(new SurfaceHolder.Callback() {
+            @Override
+            public void surfaceCreated(@NonNull SurfaceHolder holder) {
+                h264Player = new H264Player(MainActivity.this,
+                        new File(Environment.getExternalStorageDirectory()+"/test", "out3.h264").getAbsolutePath(),
+                        surfaceHolder.getSurface());
+                h264Player.play();
+            }
+            @Override
+            public void surfaceChanged(@NonNull SurfaceHolder holder, int format, int width, int height) {
+            }
+            @Override
+            public void surfaceDestroyed(@NonNull SurfaceHolder holder) {
+
+            }
+        });
+
     }
 }
